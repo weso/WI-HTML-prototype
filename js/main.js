92f2a4034ad03d4e86f0da468d7638b181749c7b
alexmontesg <alejandro.montes.g@gmail.com>
2013-05-28 10:49:15 +0200

    Added map sample

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-
+$(function() {
+	var map = new jvm.WorldMap({
+		container : $("#world-map"),
+		map : 'world_merc_en',
+		zoomOnScroll : false,
+		regionsSelectable : true,
+		regionStyle : {
+			initial : {
+				fill : '#669900'
+			},
+			selected : {
+				fill : '#177081'
+			},
+		},
+		backgroundColor : '#FFFFFF',
+		onRegionSelected : function(e, code, isSelected, selectedRegions) {
+			$('#bt_create').removeAttr('disabled')
+			if (isSelected) {
+				var countryName = map.getRegionName(code);
+			}
+		}
+	});
+});
